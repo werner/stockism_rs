95082c001181ca3a3f54b3209ea9bc80ada6032d
Werner <werner_a_e@yahoo.es>
2017-07-24 18:55:03 -0300

    Fix warning loading payload

--- src/bin/handlers/warehouses_handler.rs
+++ src/bin/handlers/warehouses_handler.rs
@@ -34,24 +34,29 @@ pub fn create(req: &mut Request) -> IronResult<Response> {
     let content_type = "application/json; charset=utf-8".parse::<Mime>().unwrap();
     let connection   = establish_connection();
     let mut payload  = String::new();
-    req.body.read_to_string(&mut payload);
-    match serde_json::from_str(&payload) {
-        Ok(request) => {
-            let warehouse: NewWarehouse = request;
-            match create_warehouse(&connection, warehouse.name) {
-                Ok(_warehouse) => Ok(Response::with((content_type, status::Ok,
-                                                    "{'sucess': true}"))),
-                Err(error)    => Ok(Response::with((content_type, status::Ok,
-                                                    format!("{{'sucess': false, message: {} }}", error)))),
+    match req.body.read_to_string(&mut payload) {
+        Ok(_) => {
+            match serde_json::from_str(&payload) {
+                Ok(request) => {
+                    let warehouse: NewWarehouse = request;
+                    match create_warehouse(&connection, warehouse.name) {
+                        Ok(_warehouse) => Ok(Response::with((content_type, status::Ok,
+                                                             "{'sucess': true}"))),
+                        Err(error)    => Ok(Response::with((content_type, status::Ok,
+                                                            format!("{{'sucess': false, message: {} }}", error)))),
+                    }
+                },
+                Err(p_error) => Ok(Response::with((content_type, status::Ok,
+                                                   format!("{{'sucess': false, message: {} }}", p_error)))),
             }
         },
-        Err(p_error) => Ok(Response::with((content_type, status::Ok,
-                                           format!("{{'sucess': false, message: {} }}", p_error)))),
+        Err(_) => Ok(Response::with((content_type, status::Ok,
+                                     "{{'sucess': false, message: error loading payload }}"))),
     }
 
 }
 
-pub fn create_warehouse<'a>(conn: &PgConnection, name: &'a str) -> QueryResult<Warehouse> {
+fn create_warehouse<'a>(conn: &PgConnection, name: &'a str) -> QueryResult<Warehouse> {
     use self::schema::warehouses;
 
     let new_warehouse = NewWarehouse {
